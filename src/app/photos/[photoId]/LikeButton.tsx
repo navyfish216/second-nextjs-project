3d512310957513c7565750d17d2d3554f2e9d2d4
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-06-27 16:41:53 +0900

    いいねボタンのアイコン表示を状態によって切り替え

--- src/app/photos/[photoId]/LikeButton.tsx
+++ src/app/photos/[photoId]/LikeButton.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { useState } from "react";
+
 export function LikeButton({ photoId }: { photoId: string }) {
+    const [liked, setLiked] = useState(false);
+
     return (
         <button onClick={() => {
             console.log(`photoId ${photoId} が「いいね」されました`);
+            setLiked(!liked);
         }}>
-            いいね
+            {liked ? '❤️' : '🤍' } いいね
         </button>
     );
 }
