a2c77a965f2afa2bb18f60deab0bdbe99f4555e7
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-06-28 16:49:12 +0900

    いいねボタンを加算式にする

--- src/app/_components/LikeButton.tsx
+++ src/app/_components/LikeButton.tsx
@@ -3,20 +3,43 @@
 import { useState } from "react";
 
 export function LikeButton({ id }: { id: string }) {
+  const [likes, setLikes] = useState(0);
   const [liked, setLiked] = useState(false);
 
+  const handleLike = () => {
+    if (liked) {
+      console.log(`LikeButton.id ${id} が「いいね」が追加されました`);
+    } else {
+      console.log(`LikeButton.id ${id} が「いいね」されました`);
+      setLiked(!liked);
+    }
+    setLikes(likes + 1);
+  };
+
+  const handleCancelLike = () => {
+    if (liked) {
+      console.log(`LikeButton.id ${id} の「いいね」が外されました`);
+      setLiked(!liked);
+      setLikes(0);
+    } else {
+      console.log(`LikeButton.id ${id} は「いいね」されていません`);
+    }
+  }
+
   return (
-    <button
-      onClick={() => {
-        if (liked) {
-          console.log(`LikeButton.id ${id} の「いいね」が外されました`);
-        } else {
-          console.log(`LikeButton.id ${id} が「いいね」されました`);
-        }
-        setLiked(!liked);
-      }}
-    >
-      {liked ? "❤️" : "🤍"} いいね
-    </button>
+    <div>
+        <div>
+          <button onClick={handleLike}>
+            {liked ? "❤️" : "🤍"} いいね {likes > 0 && `(${likes})`}
+          </button>
+        </div>
+      {liked &&
+        <div>
+          <button onClick={handleCancelLike}>
+            いいねをやめる
+          </button>
+        </div>
+      }
+    </div>
   );
 }
