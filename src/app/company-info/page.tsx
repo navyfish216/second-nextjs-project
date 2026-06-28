1ae1eef8ed7a5ce65dfe4f2d0c7b7839565d83d8
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-06-28 14:18:45 +0900

    【Next.js】修行⑦（Routeのレンダリングを静的→動的に変えてみる）

--- src/app/company-info/page.tsx
+++ src/app/company-info/page.tsx
@@ -1,4 +1,9 @@
+import { cookies } from "next/headers";
+
 export default function Page() {
+    const cookieStore = cookies();
+    console.log(`company-info.cookieStore: ${cookieStore}`);
+
     return (
         <div>
             <h1>企業概要</h1>
--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,4 +1,15 @@
-export default function Page() {
+type Props = {
+  searchParams: Promise<{[key: string]: string | string[] | undefined}>;
+}
+
+export default async function Page({searchParams}: Props) {
+  
+  if (typeof (await searchParams).foo === "string") {
+    console.log("privacy-policy.searchParams.foo: string");
+  } else {
+    console.log("privacy-policy.searchParams.foo: other");
+  }
+
   return (
     <div>
       <h1>利用規約</h1>
