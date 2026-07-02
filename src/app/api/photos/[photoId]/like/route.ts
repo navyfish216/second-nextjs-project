78b7143f8e44ff88fb588559ceb0d6c7fbec8ecb
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-07-02 20:58:29 +0900

    【Next.js】修行⑪

--- src/app/api/photos/[photoId]/like/route.ts
+++ src/app/api/photos/[photoId]/like/route.ts
@@ -2,6 +2,9 @@ export async function POST(
   _: Request,
   {params}: {params: Promise<{photoId: string}>}
 ) {
-  console.log(`photoId ${(await params).photoId} が「いいね」されました`);
-  return Response.json({liked: true});
+  const id = (await params).photoId;
+  const res = await fetch(`http://localhost:8080/api/photos/${id}/like`, {
+    method: "POST",
+  });
+  return res;
 }
