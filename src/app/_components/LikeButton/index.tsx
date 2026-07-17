9f6cb00f3ba16ab13642bcdd9b92e471e909f9d1
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-07-17 19:45:29 +0900

    useStateをひとつにまとめる

--- src/app/_components/LikeButton/index.tsx
+++ src/app/_components/LikeButton/index.tsx
@@ -6,23 +6,19 @@ import type { Like } from "@/type";
 import styles from "./style.module.css";
 
 export function LikeButton({ photoId, userId, like }: { photoId: string, userId: string, like: Like }) {
-  let likes = like.likes;
-  let liked = like.liked;
-  const [useLikes, setLikes] = useState(likes);
-  const [useLiked, setLiked] = useState(liked);
+  const [likeState, setLikeState] = useState(like);
 
   const handleLike = async () => {
     const like: Like = await postLike(photoId, userId);
     console.log(`photoId ${photoId} liked: ${like.liked}, likes: ${like.likes}`);
-    setLiked(like.liked);
-    setLikes(like.likes);
+    setLikeState(like);
   };
 
   return (
     <div className={styles.field}>
         <div>
-          <button onClick={handleLike} className={useLiked ? styles.likebutton_liked : styles.likebutton}>
-            {useLiked ? "🤍" : "❤️"} {`${useLikes}`}
+          <button onClick={handleLike} className={likeState.liked ? styles.likebutton_liked : styles.likebutton}>
+            {likeState.liked ? "🤍" : "❤️"} {`${likeState.likes}`}
           </button>
         </div>
     </div>
