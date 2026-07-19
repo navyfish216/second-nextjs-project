2f6ccbdd7c905fde2fd7e64c698ef7f156ce4dca
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-07-19 12:12:24 +0900

    いいねアイコンをsvgファイルから読み込む方式に変更

--- src/app/_components/LikeButton/index.tsx
+++ src/app/_components/LikeButton/index.tsx
@@ -18,15 +18,8 @@ export function LikeButton({ photoId, userId, like }: { photoId: string, userId:
     <div className={styles.field}>
         <div>
           <button onClick={handleLike} className={likeState.liked ? styles.like__button__liked : styles.like__button}>
-            {/* <span className={styles.icon__heart} /> {`${likeState.likes}`} */}
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" 
-                 className={likeState.liked ? styles.icon__liked : styles.icon} viewBox="0 0 16 16">
-              <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
-            </svg>
-            <span className={styles.span__likes}>
-            {`${likeState.likes}`}
-            </span>
-            {/* {likeState.liked ? "🤍" : "❤️"} {`${likeState.likes}`} */}
+            <span className={likeState.liked ? styles.icon__liked : styles.icon} />
+            <span className={styles.span__likes}>{`${likeState.likes}`}</span>
           </button>
         </div>
     </div>
